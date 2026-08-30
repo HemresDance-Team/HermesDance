7befcddabbed9b76e4a43b9a5efa58fe510d7f33
Hayk <avrecij02@gmail.com>
2026-08-30 11:16:45 +0300

    Поправил структуру Направлений

--- src/App.tsx
+++ src/App.tsx
@@ -42,24 +42,74 @@ function App() {
         <section className="directions">
           <span className="directions_text">Направления</span>
           <div className="grid">
-              <div className="cards-1">HIP-HOP<span className="card_more">Подробнее</span></div>
-              <div className="cards-1">DANCE MIX<span className="card_more">Подробнее</span></div>
-              <div className="cards-1">BREAKING<span className="card_more">Подробнее</span></div>
-              <div className="cards-1">JAZZ-FANK<span className="card_more">Подробнее</span></div>
-              <div className="cards-2">K-POP<span className="card_more">Подробнее</span></div>
-              <div className="cards-2">CONTEMPORARY<span className="card_more">Подробнее</span></div>
-              <div className="cards-2">LADIES<span className="card_more">Подробнее</span></div>
+            <div className="cards-1">
+              HIP-HOP
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
+                <br />
+                <span>То-то сё-то танцуем то-то.</span>
+              </Modal>
+            </div>
+            <div className="cards-1">
+              DANCE MIX
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
+                <br />
+                <span>То-то сё-то танцуем то-то.</span>
+              </Modal>
+            </div>
+            <div className="cards-1">
+              BREAKING
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
+                <br />
+                <span>То-то сё-то танцуем то-то.</span>
+              </Modal>
+            </div>
+            <div className="cards-1">
+              JAZZ-FANK
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
+                <br />
+                <span>То-то сё-то танцуем то-то.</span>
+              </Modal>
+            </div>
           </div>
-          <div className="directions_grid">
-            <div>
-              <span>Армянские танцы</span>
-
-              <button onClick={() => setModalOpen(true)}>Открыть направление</button>
-              <Modal
-                isOpen={isModalOpen}
-                onClose={() => setModalOpen(false)}
-                title="Армянские танцы"
-              >
+          <div className="cards2_container">
+            <div className="cards-2">
+              K-POP
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
+                <br />
+                <span>То-то сё-то танцуем то-то.</span>
+              </Modal>
+            </div>
+            <div className="cards-2">
+              CONTEMPORARY
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
+                <br />
+                <span>То-то сё-то танцуем то-то.</span>
+              </Modal>
+            </div>
+            <div className="cards-2">
+              LADIES
+              <button className="card_more" onClick={() => setModalOpen(true)}>
+                Открыть направление
+              </button>
+              <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Ladies">
                 <br />
                 <span>То-то сё-то танцуем то-то.</span>
               </Modal>
@@ -67,24 +117,69 @@ function App() {
           </div>
         </section>
         <section className="Teachers">
-            <span className="teachers_text">Наши педагоги</span>
-            <div className="grid">
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Гребнева Елизавета</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Родионова Анастасия</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Макарова Анжела</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Малова Наталья</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Матюшкина Ирина</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Маншилина Каролина</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Галкина Юлия</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Николаева Анна</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Урлапова Екатерина</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Алексеев Максим</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Маркин Роман</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Чибиряев Егор</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Федулова Таисия</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Завьялова Дарья</span></div>
-                <div className="grid_content"><img src={teacher}/><span className="teachers_name">Волкова Валерия</span></div>
+          <span className="teachers_text">Наши педагоги</span>
+          <div className="grid_teacher">
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Гребнева Елизавета</span>
             </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Родионова Анастасия</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Макарова Анжела</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Малова Наталья</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Матюшкина Ирина</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Маншилина Каролина</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Галкина Юлия</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Николаева Анна</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Урлапова Екатерина</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Алексеев Максим</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Маркин Роман</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Чибиряев Егор</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Федулова Таисия</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Завьялова Дарья</span>
+            </div>
+            <div className="grid_content">
+              <img src={teacher} />
+              <span className="teachers_name">Волкова Валерия</span>
+            </div>
+          </div>
         </section>
       </main>
     </>
